3a7408e237009e2b75e724129895c561c9a2b2f4
Pranjalchansa <pranjalchansauriya@gmail.com>
2026-04-19 03:31:25 +0530

    Solved Two Sum Target Whose sum is equal to target

---
+++ twosumtarget.java
@@ -0,0 +1,28 @@
+public class twosumtarget {
+    public static void twoSum(int nums[],int target){
+        int left=0;
+        int right=nums.length-1;
+        
+        while(left<right){
+            int sum = nums[left]+nums[right];
+
+            if (sum== target){
+                System.out.println("Indices:["+left+"],["+right+"]");
+                return;
+            }
+            if(sum<target){
+                left++;
+            }
+                else{
+                    right--;
+                }
+            }
+        System.out.println("no pair found");
+ }
+    public static void main(String args[]){
+        int nums[]={2,7,11,15};
+        int target=9;
+        twoSum(nums,target);
+    }
+    
+}
