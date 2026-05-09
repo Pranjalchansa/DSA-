e679da7d188901344725ac186d1128b5d65a2efe
Pranjalchansa <pranjalchansauriya@gmail.com>
2026-05-09 20:39:26 +0530

    Solved Q.242 Valid anagram.

---
+++ validAnagram.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class validAnagram {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : t.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) == 0) {
+                return false;
+            }
+            map.put(c, map.get(c) - 1);
+        }
+        return true;
+
+    
+}
+    public static void main(String args[]){
+        String s="anagram";
+        String t="nagaram";
+        System.out.println(isAnagram(s, t));
+    }
+}
