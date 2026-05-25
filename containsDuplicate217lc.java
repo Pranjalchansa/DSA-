f91a14b0de98be38cc23697f674ccb87d80053cc
Pranjalchansa <pranjalchansauriya@gmail.com>
2026-05-25 23:49:30 +0530

    Solved Q217. Contains Duplicate - leetcode

---
+++ containsDuplicate217lc.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+class containsDuplicate217lc {
+
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (set.contains(nums[i])) {
+                return true;
+            }
+
+            set.add(nums[i]);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        containsDuplicate217lc obj = new containsDuplicate217lc();
+
+        int[] nums = {1, 2, 3, 1};
+
+        boolean result = obj.containsDuplicate(nums);
+
+        System.out.println(result);
+    }
+}
