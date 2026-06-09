4274e5a4b6ce109f7211b7409d352e75d6839a2f
Pranjalchansa <pranjalchansauriya@gmail.com>
2026-06-09 16:13:32 +0530

    Solved Q3300. Minimum Element After Replacement With Digit Sum.

---
+++ Q3300lc.java
@@ -0,0 +1,19 @@
+public class Q3300lc {
+    public static int minElement(int nums[]){
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            int num=nums[i];
+            int sum=0;
+            while(num>0){
+                sum += num%10;
+                num/=10;
+            }
+            min=Math.min(min,sum);
+        }
+        return min;
+    }
+    public static void main(String args[]){
+        int nums[]={10,12,13,14};
+        System.out.println(minElement(nums));
+}
+}
