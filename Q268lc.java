e9474ef21e0a8c0fb7fd616f5cef24c181df93d5
Pranjalchansa <pranjalchansauriya@gmail.com>
2026-06-18 15:22:25 +0530

    Solved Q268. Missing Number in leetcode.

---
+++ Q268lc.java
@@ -0,0 +1,15 @@
+public class Q268lc {
+    public static int missingNumber(int nums[]){
+        int n=nums.length;
+        int expectedsum = n*(n+1)/2;
+        int actualsum = 0;
+        for(int i=0;i<n;i++){
+            actualsum+=nums[i];
+        }
+        return expectedsum-actualsum;
+    }
+    public static void main(String[] args) {
+        int nums[]={3,0,1};
+        System.out.println(missingNumber(nums));
+    }
+}
